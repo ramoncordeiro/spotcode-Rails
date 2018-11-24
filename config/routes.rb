68aee6ead3f35c651e9757a231bd9c18e5d1c24c
ramoncord <ramonestii@hotmail.com>
2018-11-23 22:48:38 -0300

    category and artists pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 =begin
 Rails.application.routes.draw do
+  get 'artists/show'
+  get 'categories/show'
   get 'search/index'
   get 'search/new'
   get 'dashboard/index'
@@ -11,6 +13,8 @@ end
 =end
 
 Rails.application.routes.draw do
+  #get 'artists/show'
+  #get 'categories/show'
   #get 'search/index'
   #get 'search/new'
   devise_for :users
@@ -18,6 +22,8 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
+    resources :categories, only: :show
+    resources :artists, only: :show
   end
  
   unauthenticated :user do

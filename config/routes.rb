ac9a7688e8ce4fcf648cec11490a3ede4907bc3d
ramoncord <ramonestii@hotmail.com>
2018-11-24 21:23:29 -0300

    execute music play done

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,5 @@
-=begin
 Rails.application.routes.draw do
-  get 'favorites/index'
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'albums/show'
-  get 'artists/show'
-  get 'categories/show'
-  get 'search/index'
-  get 'search/new'
-  get 'dashboard/index'
-  devise_for :users
-  #get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
-end
-=end
-
-Rails.application.routes.draw do
-  #get 'favorites/index'
-  #get 'favorites/create'
-  #get 'favorites/destroy'
-  #get 'albums/show'
-  #get 'artists/show'
-  #get 'categories/show'
-  #get 'search/index'
-  #get 'search/new'
+  get 'recently_heards/create'
   devise_for :users
   
   authenticated :user do
@@ -33,7 +8,9 @@ Rails.application.routes.draw do
     resources :search, only: [:index, :new], as: :searches
     resources :categories, only: :show
     resources :artists, only: :show
-    resources :albums, only: :show
+    resources :albums, only: :show do
+      resources :recently_heards, only: :create
+    end
     resources :songs, only: [] do
       post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
       delete "/favorite", to: "favorites#destroy", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
